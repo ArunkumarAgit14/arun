819f5e727d37e4913e4ef005913c932130b6a701
ArunkumarAgit14 <133665109+ArunkumarAgit14@users.noreply.github.com>
2023-05-15 18:56:30 +0530

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: ["@vue/cli-plugin-babel/preset"]
+};
+
+// const GoogleFontsPlugin = require('google-fonts-webpack-plugin')
+
+// module.exports = {
+//   presets: [
+//     '@vue/app',
+//   ],
+//   plugins: [
+//     new GoogleFontsPlugin({
+//       fonts: [
+//         { family: 'Roboto' },
+
+//       ],
+//       /* ...options */
+//     }),
+//   ],
+// };
